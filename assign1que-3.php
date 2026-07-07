90489fe11b81f64248a7c701cca1cb79668e89af
arshima-pathan <aashimak031@gmail.com>
2026-07-07 09:06:42 +0530

    Add files via upload

---
+++ assign1que-3.php
@@ -0,0 +1,50 @@
++<html>
+<head>
+   <title>ass-1 q-3</title>
+</head>
+<body>
+
+<?php
+  $a=10;
+  $b=20;
+?>
+<table align="center" border="1" height="30%" width="40%">
+
+  <tr>
+<td><b>operations</b></td>
+<td><b>answers</b></td>
+  </tr>  
+  
+   <tr>
+<td><b>value of A:</b></td>
+<td><?php echo $a ?></td>
+  </tr>  
+  
+   <tr>
+<td><b>value of b:</b></td>
+<td><?php echo $b ?></td>
+  </tr>  
+  
+    <tr>
+<td><b>Addition of A and B:</b></td>
+<td><?php echo ($a+$b) ?></td>
+  </tr>  
+  
+    <tr>
+<td><b>substraction of A and B:</b></td>
+<td><?php echo ($a-$b) ?></td>
+  </tr>  
+  
+      <tr>
+<td><b>multiplication of A and B:</b></td>
+<td><?php echo ($a*$b) ?></td>
+  </tr>  
+  
+      <tr>
+<td><b>division of A and B:</b></td>
+<td><?php echo ($a/$b) ?></td>
+  </tr>  
+  
+  </table>
+  </body>
+  </html>

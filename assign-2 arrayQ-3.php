f40846ae8609b57d8014cf4d0ef6cfbdee36923e
arshima-pathan <aashimak031@gmail.com>
2026-07-16 10:39:46 +0530

    Add files via upload

---
+++ assign-2 arrayQ-3.php
@@ -0,0 +1,11 @@
+<?php
+$a = array("japan"=>"tokyo","mexico"=>"mexico city","usa"=>"new york city","india"=>"mumbai", "korea"=>"seoul",
+"chaina"=>"shanghai", "nigeria"=>"lagos", " Argentina"=>"Buenos Aires", "Egypt"=>"Cairo", "England"=>"london");
+foreach($a as $k=>$v)
+{
+echo $v." is in ".$k."<br>";
+}
+?>
+
+
+
---
+++ assign-2 arrayQ-4.php
@@ -0,0 +1,9 @@
+<?php
+$a=array("Atmiya", "Christ","DDIT","ROLLWALA","Marwadi","RK");
+
+foreach($a as $clg)
+{
+echo $clg."<br>";	
+}	
+
+?>

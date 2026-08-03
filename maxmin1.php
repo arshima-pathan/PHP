1fee9cf2b9f29e3a6ebd50e37c2d65d104280ca7
arshima-pathan <aashimak031@gmail.com>
2026-08-03 11:46:50 +0530

    Add files via upload

---
+++ maxmin1.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<form action=maxmin2.php method="get">
+Enter value 1:<input type="number" name="v1"><br>
+Enter value 2:<input type="number" name="v2"><br>
+Enter value 3:<input type="number" name="v3"><br>
+<input type="submit" name="sbt1" value="max">
+<input type="submit" name="sbt2" value="min">
+
+</form>
+</h

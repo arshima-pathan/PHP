3c19d08af7761ef425c08f57c45318af72f696f6
arshima-pathan <aashimak031@gmail.com>
2026-08-20 10:47:00 +0530

    Add files via upload

---
+++ file_1.php
@@ -0,0 +1,7 @@
+<html>
+<form action='file_2.php' method='POST' enctype='multipart/form-data'>
+select any file:
+<input type='file' name='f1'><br>
+<input type='submit' value='upload file'>
+</form>
+</html>
